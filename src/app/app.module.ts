15753b3b20e53890661105b94a15f65c3c8f135b
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2018-02-16 18:32:43 -0500

    Add navigation module and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RacesAndEventsComponent } from './races-and-events/races-and-events.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { InitiativesComponent } from './initiatives/initiatives.component';
+import { FaqComponent } from './faq/faq.component';
+import { PhotosAndStoriesComponent } from './photos-and-stories/photos-and-stories.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RacesAndEventsComponent,
+    ResourcesComponent,
+    InitiativesComponent,
+    FaqComponent,
+    PhotosAndStoriesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
